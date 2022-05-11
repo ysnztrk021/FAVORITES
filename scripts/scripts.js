8f5e1db1a45102cf2bc785430f91352e873f9c2f
ysnztrk021 <yamoz1030@gmail.com>
2022-05-11 20:33:18 +0200

    site modified

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,212 +2,203 @@ const favourite_webtoon = [
     {
         webtoon: "Murim Login",
         link_webtoon: "https://www.asurascans.com/comics/murim-login/",
-        img_webtoon:"img/Murim-login.jpg"
+        img_webtoon: "https://www.asurascans.com/wp-content/uploads/2022/04/LoginMoorimCover03.png"
     },
     {
         webtoon: "Demonic Emperor",
         link_webtoon: "https://alpha-scans.org/manga/demonic-emperor/",
-        img_webtoon:"img/Demonic-emperor.jpg"
+        img_webtoon: "https://i2.wp.com/alpha-scans.org/wp-content/uploads/2022/01/demonic-emperor.jpg"
     },
     {
         webtoon: "Player Who Can't Level Up",
         link_webtoon: "https://toonily.com/webtoon/the-player-that-cant-level-up/",
-        img_webtoon:"img/Player-who-can't-level-up.jpg"
+        img_webtoon: "img/Player-who-can't-level-up.jpg"
     },
     {
         webtoon: "Reincarnation of the Suicidal Battle God",
         link_webtoon: "https://www.asurascans.com/comics/367-reincarnation-of-the-suicidal-battle-god/",
-        img_webtoon:"img/Reincarnation-of-the-suicidal-battle-god.jpg"
+        img_webtoon: "https://www.asurascans.com/wp-content/uploads/2021/06/sogdobogdo.jpg"
     },
     {
         webtoon: "Taming Master",
         link_webtoon: "https://toonily.com/webtoon/taming-master/",
-        img_webtoon:"img/Taming-master.jpg"
+        img_webtoon: "img/Taming-master.jpg"
     },
     {
         webtoon: "The Max Level Hero has Returned!",
         link_webtoon: "https://toonily.com/webtoon/the-max-level-hero-has-returned/",
-        img_webtoon:"img/Max-level-hero-has-returned.jpg"
+        img_webtoon: "img/Max-level-hero-has-returned.jpg"
     },
     {
         webtoon: "Return Of The Shattered Constellation",
         link_webtoon: "https://www.asurascans.com/comics/1649969363-return-of-the-shattered-constellation/",
-        img_webtoon:"img/Return-of-the-shattered-constellation.jpg"
+        img_webtoon: "https://www.asurascans.com/wp-content/uploads/2021/11/unknown.png"
     },
     {
         webtoon: "Jujutsu Kaisen",
         link_webtoon: "https://readmanganato.com/manga-ba979135",
-        img_webtoon:"img/Jjk.jpg"
+        img_webtoon: "https://avt.mkklcdnv6temp.com/30/d/16-1583494554.jpg"
     },
     {
         webtoon: "Mercenary Enrollment",
         link_webtoon: "https://readmanganato.com/manga-jz987182",
-        img_webtoon:"img/Mercenary-enrollment.jpg"
+        img_webtoon: "https://avt.mkklcdnv6temp.com/36/y/22-1605087752.jpg"
     },
     {
         webtoon: "Unordinary",
         link_webtoon: "https://readmanganato.com/manga-uw971531",
-        img_webtoon:"img/Unordinary.jpg"
+        img_webtoon: "https://avt.mkklcdnv6temp.com/47/j/13-1583489495.jpg"
     },
     {
         webtoon: "Ranker Who Lives A Second Time",
         link_webtoon: "https://toonily.com/webtoon/second-ranker/",
-        img_webtoon:"img/Second-Life-Ranker.jpg"
+        img_webtoon: "img/Second-Life-Ranker.jpg"
     },
 
     {
         webtoon: "The Beginning After The End",
         link_webtoon: "https://toonily.com/webtoon/beginning-after-end/",
-        img_webtoon:"img/The-Beginning-After-the-End.jpg"
+        img_webtoon: "img/The-Beginning-After-the-End.jpg"
     },
     {
         webtoon: "Memorize",
         link_webtoon: "https://luminousscans.com/series/manhwa-of-memorize-novel/",
-        img_webtoon:"img/Memorize.jpg"
+        img_webtoon: "https://luminousscans.com/wp-content/uploads/2021/07/resource.jpg"
     },
     {
         webtoon: "FFF-Class Trash Hero",
         link_webtoon: "https://toonily.com/webtoon/fff-class-trashero/",
-        img_webtoon:"img/FFF-Class-Trashero.jpg"
+        img_webtoon: "img/FFF-Class-Trashero.jpg"
     },
     {
         webtoon: "Omniscient Reader’s Viewpoint",
         link_webtoon: "https://manhwaz.com/webtoon/omniscient-readers-viewpoint-006",
-        img_webtoon:"img/Omniscient-reader's-viewpoint.jpg"
+        img_webtoon: "img/Omniscient-reader's-viewpoint.jpg"
     },
     {
         webtoon: "Apotheosis",
         link_webtoon: "https://readmanganato.com/manga-bt978676",
-        img_webtoon:"img/Apotheosis.jpg"
+        img_webtoon: "https://avt.mkklcdnv6temp.com/13/f/16-1583493949.jpg"
     },
     {
         webtoon: "Martial Peak",
         link_webtoon: "https://manhuaplus.com/manga/martial-peak/",
-        img_webtoon:"img/Martial-peak.jpg"
+        img_webtoon: "https://manhuaplus.com/wp-content/uploads/2020/07/MP-193x278.jpg"
     },
     {
         webtoon: "Soul Land",
         link_webtoon: "https://www.whitecloudpavilion.com/manga/patreon/manga/soul-land",
-        img_webtoon:"img/Soul-land.jpg"
+        img_webtoon: "https://whitecloudpavilion.com/manga/patreon/uploads/manga/soul-land/cover/cover_250x350.jpg"
     },
     {
         webtoon: "Eleceed",
         link_webtoon: "https://toonily.com/webtoon/eleceed/",
-        img_webtoon:"img/Eleceed.jpg"
+        img_webtoon: "img/Eleceed.jpg"
     },
     {
         webtoon: "Tales of Demons and Gods",
         link_webtoon: "https://setsuscans.com/manga/the-tales-of-demons-and-gods/",
-        img_webtoon:"img/Tales-of-demons-and-gods.jpg"
+        img_webtoon: "https://i0.wp.com/setsuscans.com/wp-content/uploads/2021/04/001_0-scaled.jpg?fit=239%2C350&ssl=1"
     },
     {
         webtoon: "Tomb Raider King",
         link_webtoon: "https://toonily.com/webtoon/tomb-raider-king/",
-        img_webtoon:"img/Tomb-Raider-King.jpg"
+        img_webtoon: "img/Tomb-Raider-King.jpg"
     },
     {
         webtoon: "Swordmaster’s Youngest Son",
         link_webtoon: "https://www.asurascans.com/comics/swordmasters-youngest-son/",
-        img_webtoon:"img/Swordmaster's-youngest-son.jpg"
+        img_webtoon: "https://www.asurascans.com/wp-content/uploads/2022/04/cover.jpg"
     },
     {
         webtoon: "Return of the Disaster-Class Hero",
         link_webtoon: "https://www.asurascans.com/comics/1649969363-return-of-the-disaster-class-hero/",
-        img_webtoon:"img/Return-of-the-disaster-class-hero.jpg"
+        img_webtoon: "https://www.asurascans.com/wp-content/uploads/2021/10/2_-_Copy_copy.jpg"
     },
 ]
 const favourite_site = [
     {
         constantly: "YouTube",
         link_constantly: "https://www.youtube.com/",
-        img_constantly:"img/youtube.png"
-    }, 
+        img_constantly: "http://assets.stickpng.com/images/580b57fcd9996e24bc43c545.png"
+    },
     {
         constantly: "Facebook",
         link_constantly: "https://www.facebook.com/",
-        img_constantly:"img/facebook.png"
-    }, 
+        img_constantly: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1365px-Facebook_f_logo_%282019%29.svg.png"
+    },
     {
         constantly: "Gmail",
         link_constantly: "https://mail.google.com",
-        img_constantly:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/800px-Gmail_icon_%282020%29.svg.png"
-    }, 
+        img_constantly: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/800px-Gmail_icon_%282020%29.svg.png"
+    },
     {
         constantly: "Netflix",
         link_constantly: "https://www.netflix.com/",
-        img_constantly:"https://www.dafont.com/forum/attach/orig/9/3/930608.jpg"
-    }, 
+        img_constantly: "https://www.dafont.com/forum/attach/orig/9/3/930608.jpg"
+    },
     {
         constantly: "Twitch",
         link_constantly: "https://www.twitch.tv/",
-        img_constantly:"https://cdn.pixabay.com/photo/2021/12/10/16/38/twitch-6860918_960_720.png"
-    }, 
+        img_constantly: "https://cdn.pixabay.com/photo/2021/12/10/16/38/twitch-6860918_960_720.png"
+    },
     {
         constantly: "GitHub",
         link_constantly: "https://www.github.com/",
-        img_constantly:"https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
-    }, 
+        img_constantly: "https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
+    },
     {
         constantly: "Instagram",
         link_constantly: "https://www.instagram.com/",
-        img_constantly:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png"
-    }, 
+        img_constantly: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png"
+    },
     {
         constantly: "AniMix Play",
         link_constantly: "https://animixplay.to/",
-        img_constantly:"https://animixplay.to/icon.png"
-    }, 
+        img_constantly: "https://animixplay.to/icon.png"
+    },
     {
         constantly: "Font Squirrel",
         link_constantly: "https://www.fontsquirrel.com/",
-        img_constantly:"https://iconape.com/wp-content/files/gj/58740/svg/font-squirrel.svg"
-    }, 
+        img_constantly: "https://iconape.com/wp-content/files/gj/58740/svg/font-squirrel.svg"
+    },
     {
         constantly: "Twitter",
         link_constantly: "https://www.twitter.com/",
-        img_constantly:"https://upload.wikimedia.org/wikipedia/fr/thumb/c/c8/Twitter_Bird.svg/1200px-Twitter_Bird.svg.png"
+        img_constantly: "https://upload.wikimedia.org/wikipedia/fr/thumb/c/c8/Twitter_Bird.svg/1200px-Twitter_Bird.svg.png"
     },
     {
         constantly: "Xbox Cloud Game",
         link_constantly: "https://www.xbox.com/fr-BE/play",
-        img_constantly:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png"
+        img_constantly: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/2048px-Xbox_one_logo.svg.png"
     },
 ]
 
-const list_webtoon = document.querySelector(".list-webtoon");
+const list_webtoon = document.querySelector(".list-webtoon-grid");
 const list_site = document.querySelector(".list-site");
 
 const sorted_webtoon = favourite_webtoon.sort(sortWebtoon)
 const sorted_constantly = favourite_site.sort(sortSite)
 
 for (let i = 0; i < sorted_webtoon.length; i++) {
-    list_webtoon.innerHTML += 
-    `
-    <div class="webtoon">
-    <div class="webtoon-img">
-    <a target="_blank" href="${sorted_webtoon[i].link_webtoon}" title="${sorted_webtoon[i].webtoon}">
-    <img src="${sorted_webtoon[i].img_webtoon}" alt="${sorted_webtoon[i].webtoon}">
-    </a>
-    </div>
-    <div class="webtoon-name">
-    <a target="_blank" href="${sorted_webtoon[i].link_webtoon}">${sorted_webtoon[i].webtoon}</a>
-    </div>
-    </div>
+    list_webtoon.innerHTML +=
+        `
+    <li>
+	<a target="_blank" href="${sorted_webtoon[i].link_webtoon}">
+	<img src="${sorted_webtoon[i].img_webtoon}" alt="" srcset="">
+	<div class="title-webtoon">
+		${sorted_webtoon[i].webtoon}
+	</div>
+	</a>
+	</li>
     `
 }
 for (let i = 0; i < sorted_constantly.length; i++) {
     list_site.innerHTML += 
     `
-    <div class="constantly">
-    <div class="constantly-img">
-    <a target="_blank" href="${sorted_constantly[i].link_constantly}" title="${sorted_constantly[i].constantly}">
-    <img src="${sorted_constantly[i].img_constantly}" href="${sorted_constantly[i].link_constantly}">
-    </a>
-    </div>
-    <div class="constantly-name">
-    <a target="_blank" href="${sorted_constantly[i].link_constantly}">${sorted_constantly[i].constantly}</a>
-    </div>
-    </div>
+	<li>
+	<a target="_blank" href="${sorted_constantly[i].link_constantly}"><img src="${sorted_constantly[i].img_constantly}" alt="" srcset=""> ${sorted_constantly[i].constantly}</a>
+	</li>
     `
 }
 
