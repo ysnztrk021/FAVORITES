4db4c5d07fdc47f32264ac0b820d4615b6bbc87b
ysnztrk021 <yamoz1030@gmail.com>
2022-06-10 08:34:16 +0200

    changed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -72,7 +72,7 @@ const favourite_webtoon = [
     },
     {
         webtoon: "Omniscient Reader’s Viewpoint",
-        link_webtoon: "https://manhwaz.com/webtoon/omniscient-readers-viewpoint-007",
+        link_webtoon: "https://manhwaz.com/webtoon/omniscient-readers-viewpoint-009",
         img_webtoon: "img/Omniscient-reader's-viewpoint.jpg"
     },
     {
